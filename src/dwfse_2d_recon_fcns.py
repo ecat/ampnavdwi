e456271d33a7abde0277341cf9419a0e20732472
root <pkllee@stanford.edu>
2024-09-16 08:04:49 +0000

    Fixed dimensions

--- src/dwfse_2d_recon_fcns.py
+++ src/dwfse_2d_recon_fcns.py
@@ -10,70 +10,69 @@ class SensitivityDataSource(enum.Enum):
     FULL_LOW_BVALUE_IMAGE = 1
 
 
-def calibration_data_sorting(ksp_cal_z_sxyc, sequence_opts):
-    print(ksp_cal_z_sxyc.shape)
+def calibration_data_sorting(ksp_cal_zsxyc, sequence_opts):
+    print(ksp_cal_zsxyc.shape)
 
     n_cal_shots = sequence_opts["n_cal_scans"]
     cal_shot_split = sequence_opts["cal_shot_split"]
     print('n_cal_shots = ', n_cal_shots, 'cal_shot_split = ', cal_shot_split)
 
-    ksp_cal_sz_xyc_raw = np.transpose(ksp_cal_z_sxyc, (2, 0, 1, 3, 4, 5))
+    ksp_cal_szxyc_raw = np.transpose(ksp_cal_zsxyc, (1, 0, 2, 3, 4))
 
-    ksp_cal_sz_xyc = ksp_cal_sz_xyc_raw[0::cal_shot_split, ...] + ksp_cal_sz_xyc_raw[1::cal_shot_split, ...]
-    print(ksp_cal_sz_xyc.shape)
+    ksp_cal_szxyc = ksp_cal_szxyc_raw[0::cal_shot_split, ...] + ksp_cal_szxyc_raw[1::cal_shot_split, ...]
+    print(ksp_cal_szxyc.shape)
 
-    n_cal_shots = ksp_cal_sz_xyc.shape[0]
+    n_cal_shots = ksp_cal_szxyc.shape[0]
 
-    ksp_cal_sz_xyc[1::2, ...] = np.flip(ksp_cal_sz_xyc[1::2, ...], axis=-3)
+    ksp_cal_szxyc[1::2, ...] = np.flip(ksp_cal_szxyc[1::2, ...], axis=-3)
 
-    return ksp_cal_sz_xyc, n_cal_shots
+    return ksp_cal_szxyc, n_cal_shots
 
-def fista_2d_fse_recon(ksp_zcs_xy, im_phasenav_zsxy, sens_zcxy, mask_zs_xy, 
+def fista_2d_fse_recon(ksp_zcsxy, im_phasenav_zsxy, sens_zcxy, mask_zsxy, 
                        im_phasenav_magnitude_weights_zsxy=None, do_sample_density_preconditioner=False, max_iter=100, lamda=5e-4):
     
-    nz, nc, ns, _, nx, ny = ksp_zcs_xy.shape
-    recon_device = sp.get_device(ksp_zcs_xy)
+    nz, nc, ns, nx, ny = ksp_zcsxy.shape
+    recon_device = sp.get_device(ksp_zcsxy)
 
     xp = recon_device.xp
 
-    F = sp.linop.FFT((nc, ns, 1, nx, ny), axes=(-2, -1))
+    F = sp.linop.FFT((nc, ns, nx, ny), axes=(-2, -1))
 
-    support_mask_xy = xp.sum(xp.reshape(mask_zs_xy[0, :, 0, ...], (ns, 1, ny)), axis=0)
+    support_mask_xy = xp.sum(xp.reshape(mask_zsxy[0, :, 0, ...], (ns, 1, ny)), axis=0)
     samp_index = xp.nonzero(support_mask_xy[0, :])
     support_mask_xy = xp.zeros((nx, ny), xp.float32)
     support_mask_xy[:, xp.min(samp_index[0]):xp.max(samp_index[0])] = 1
 
-    Wav = sp.linop.Wavelet((nx, ny, 1), axes=(0, 1)) 
-    im_z_xy_recon = xp.zeros((nz, 1, nx, ny), dtype=np.complex64)
-    im_z_xy_adjoint_recon = xp.zeros_like(im_z_xy_recon)
-    T = sp.linop.Transpose((nx, ny, 1), axes=(2, 0, 1))
+    Wav = sp.linop.Wavelet((nx, ny), axes=(0, 1)) 
+    im_zxy_recon = xp.zeros((nz, nx, ny), dtype=np.complex64)
+    im_zxy_adjoint_recon = xp.zeros_like(im_zxy_recon)
 
     for z in range(nz):
-        phasenav_s_xy = xp.exp(1j * xp.angle(im_phasenav_zsxy[z, ...]))[:, xp.newaxis, :]
+        phasenav_sxy = xp.exp(1j * xp.angle(im_phasenav_zsxy[z, ...]))
 
         if im_phasenav_magnitude_weights_zsxy is not None:
-            phasenav_s_xy = phasenav_s_xy * im_phasenav_magnitude_weights_zsxy[z, :, xp.newaxis, ...]
+            phasenav_sxy = phasenav_sxy * im_phasenav_magnitude_weights_zsxy[z, :, ...]
 
-        P = sp.linop.Multiply((1, nx, ny), phasenav_s_xy)
-        sens_c__xy = sens_zcxy[z, ...][:, xp.newaxis, xp.newaxis, ...]
-        S = sp.linop.Multiply((ns, 1, nx, ny), sens_c__xy)
-        mask_s_xy = mask_zs_xy[z, ...]
-        D = sp.linop.Multiply((nc, ns, 1, nx, ny), mask_s_xy)
+        P = sp.linop.Multiply((1, nx, ny), phasenav_sxy)
+        sens_c_xy = sens_zcxy[z, ...][:, xp.newaxis, ...]
+        S = sp.linop.Multiply((ns, nx, ny), sens_c_xy)
+        mask_s_xy = mask_zsxy[z, ...]
+        D = sp.linop.Multiply((nc, ns, nx, ny), mask_s_xy)
 
         if do_sample_density_preconditioner:
             dcf___xy = xp.mean(mask_s_xy, axis=0, keepdims=False)
             dcf___xy[dcf___xy == 0] = 1
-            W = sp.linop.Multiply((nc, ns, 1, nx, ny), 1.0 / xp.sqrt(xp.reshape(dcf___xy, (1, 1, 1, 1, ny))))
+            W = sp.linop.Multiply((nc, ns, nx, ny), 1.0 / xp.sqrt(xp.reshape(dcf___xy, (1, 1, 1, ny))))
         else:
-            W = sp.linop.Identity((nc, ns, 1, nx, ny))
+            W = sp.linop.Identity((nc, ns, nx, ny))
 
-        E = sp.linop.Compose((W, D, F, S, P, T))        
+        E = sp.linop.Compose((W, D, F, S, P))        
         EHE = sp.linop.Compose((E.H, E))
 
-        ksp_csexy = xp.ascontiguousarray(ksp_zcs_xy[z, ...])
+        ksp_csexy = xp.ascontiguousarray(ksp_zcsxy[z, ...])
         Y = W * ksp_csexy
         EHY = E.H * Y
-        im_z_xy_adjoint_recon[z, ...] = xp.transpose(EHY, (2, 0, 1))
+        im_zxy_adjoint_recon[z, ...] = EHY
 
         max_eigenvalue = sp.app.MaxEig(EHE, device=recon_device, max_iter=30, dtype=xp.complex64, show_pbar=False).run()
         fista_step_size = .99 / max_eigenvalue
@@ -85,7 +84,7 @@ def fista_2d_fse_recon(ksp_zcs_xy, im_phasenav_zsxy, sens_zcxy, mask_zs_xy,
             n_circshifts = 4
             y = 0
             for _ in range(n_circshifts):
-                C = get_random_circle_shift((nx, ny, 1), (4, 4, 4))
+                C = get_random_circle_shift((nx, ny), (4, 4))
                 Cx = C * x                
                 y += C.H * prox_spatial_wav(alpha, Cx)
             return y / n_circshifts
@@ -104,18 +103,18 @@ def fista_2d_fse_recon(ksp_zcs_xy, im_phasenav_zsxy, sens_zcxy, mask_zs_xy,
             while not alg.done():
                 alg.update()
         
-        im_recon = xp.transpose(x, (2, 0, 1))
-        im_z_xy_recon[z, ...] = sp.ifft(sp.fft(im_recon, axes=(-1, -2)) * support_mask_xy, axes=(-1, -2))
+        im_recon = x
+        im_zxy_recon[z, ...] = sp.ifft(sp.fft(im_recon, axes=(-1, -2)) * support_mask_xy, axes=(-1, -2))
         
-    return im_z_xy_recon, im_z_xy_adjoint_recon
+    return im_zxy_recon, im_zxy_adjoint_recon
 
 
-def extract_full_kspace(ksp_vz_sxrc, fse_control_table_esv, ny):
-    nv, nz, _, nshots, nx, etl, nc = ksp_vz_sxrc.shape
-    ksp_vz_sxyc = np.zeros((nv, nz, 1, nshots, nx, ny, nc), dtype=np.complex64)
+def extract_full_kspace(ksp_vzsxrc, fse_control_table_esv, ny):
+    nv, nz, nshots, nx, etl, nc = ksp_vzsxrc.shape
+    ksp_vzsxyc = np.zeros((nv, nz, nshots, nx, ny, nc), dtype=np.complex64)
     for v in range(nv):
         for s in range(nshots):
             for fse_echo in range(etl):
                 pe_index = int(fse_control_table_esv[fse_echo, s, v])
-                ksp_vz_sxyc[v, :, :, s, :, pe_index, :] = ksp_vz_sxrc[v, :, :, s, :, fse_echo, :]
-    return ksp_vz_sxyc
+                ksp_vzsxyc[v, :, s, :, pe_index, :] = ksp_vzsxrc[v, :, s, :, fse_echo, :]
+    return ksp_vzsxyc
--- src/main.py
+++ src/main.py
@@ -95,21 +95,27 @@ print('Data directory: ' + str(cfl_root_dir))
 # +
 ksp_vz_sxrc, mask_vz_sxy, kspace_cal_z_sxrc, phase_encode_table_esv, phase_encode_table_cal_es, sequence_opts = cfl_exchanger.load_raw_cfls(data_dir)
 
-nv, nz, _, ns, nx, fse_etl, nc = ksp_vz_sxrc.shape
+mask_vzsxy = mask_vz_sxy[:, :, 0, ...]
+ksp_vzsxrc = ksp_vz_sxrc[:, :, 0, ...]
+kspace_cal_zsxrc = kspace_cal_z_sxrc[:, 0, ...]
+
+nv, nz, ns, nx, fse_etl, nc = ksp_vzsxrc.shape
 ny = sequence_opts["matrix_size"][1]
 valid_shot_slicer_v = sequence_opts["valid_shots_range"]
 
 
-ksp_vz_sxyc = extract_full_kspace(ksp_vz_sxrc, phase_encode_table_esv, ny)
-ksp_cal_z_sxyc = np.squeeze(extract_full_kspace(kspace_cal_z_sxrc[np.newaxis, ...], phase_encode_table_cal_es[..., np.newaxis], ny), axis=0)
+ksp_vzsxyc = extract_full_kspace(ksp_vzsxrc, phase_encode_table_esv, ny)
+ksp_cal_zsxyc = np.squeeze(extract_full_kspace(kspace_cal_zsxrc[np.newaxis, ...], phase_encode_table_cal_es[..., np.newaxis], ny), axis=0)
 
 del ksp_vz_sxrc, kspace_cal_z_sxrc
 
 do_fermi_filter = True
 if do_fermi_filter:
     # do inline so that avoids big copy
-    filter_along_y = lambda x: np.multiply(x, np.reshape(get_fermi_filter(ny), (1, 1, 1, 1, -1, 1)), x)
-    filter_along_y(ksp_vz_sxyc)
+    filter_along_y = lambda x: np.multiply(x, np.reshape(get_fermi_filter(ny), (1, 1, 1, -1, 1)), x)
+    filter_along_y(ksp_vzsxyc)
+
+
 
 
 # +
@@ -118,39 +124,39 @@ start = time.time()
 xp = recon_device.xp
 print('Run reconstruction with', recon_device)
 
-nv, nz, _, ns, nx, ny, nc = ksp_vz_sxyc.shape
+nv, nz, ns, nx, ny, nc = ksp_vzsxyc.shape
 print('matrix size: nv nz ns nx ny nc', nv, nz, ns, nx, ny, nc)
 print('ns_valid ', [len(v) for v in valid_shot_slicer_v]) # may not align with ns if montecarlo
 
 # +
 # load calibration data
-ksp_cal_sz_xyc, n_cal_shots = calibration_data_sorting(ksp_cal_z_sxyc, sequence_opts)
+ksp_cal_szxyc, n_cal_shots = calibration_data_sorting(ksp_cal_zsxyc, sequence_opts)
 
 if is_jupyter:
-    im_cal_sz_xyc = sp.ifft(ksp_cal_sz_xyc, axes=(-2, -3))
-    im_cal_sz_xy = sos(im_cal_sz_xyc, axis_in=-1)
-    im_cal_inner_product_z_xyc = im_cal_sz_xyc[0, ...] * np.conj(im_cal_sz_xyc[1, ...])
+    im_cal_szxyc = sp.ifft(ksp_cal_szxyc, axes=(-2, -3))
+    im_cal_szxy = sos(im_cal_szxyc, axis_in=-1)
+    im_cal_inner_product_z_xyc = im_cal_szxyc[0, ...] * np.conj(im_cal_szxyc[1, ...])
     sl_to_plot = 0
-    pl.ImagePlot(im_cal_sz_xy[0, :, 0, ...], z=-3, x=-2, y=-1, title='cal 0')
+    pl.ImagePlot(im_cal_szxy[0, ...], z=-3, x=-2, y=-1, title='cal 0')
 
 # +
-dcf_vz__xy_ = np.sum(mask_vz_sxy[..., np.newaxis], 3, keepdims=True)
-dcf_vz__xy_[dcf_vz__xy_ == 0] = 1
-ksp_naive_vz_xyc = np.sum(ksp_vz_sxyc / dcf_vz__xy_, axis=3)
-im_naive_vz_xyc = sp.ifft(ksp_naive_vz_xyc, axes=(-3, -2))
-im_naive_vz_xy = sos(im_naive_vz_xyc, axis_in=-1)
+dcf_vz_xy_ = np.sum(mask_vzsxy[..., np.newaxis], 2, keepdims=True)
+dcf_vz_xy_[dcf_vz_xy_ == 0] = 1
+ksp_naive_vzxyc = np.sum(ksp_vzsxyc / dcf_vz_xy_, axis=2)
+im_naive_vzxyc = sp.ifft(ksp_naive_vzxyc, axes=(-3, -2))
+im_naive_vzxy = sos(im_naive_vzxyc, axis_in=-1)
 
-normalize_scale = np.max(np.abs(im_naive_vz_xy))
+normalize_scale = np.max(np.abs(im_naive_vzxy))
 print('normalize scale', normalize_scale)
-ksp_vz_sxyc = ksp_vz_sxyc / normalize_scale
+ksp_vzsxyc = ksp_vzsxyc / normalize_scale
 
 if is_jupyter:
     sl_to_show = range(0, nz, 8)
 
     for vv in range(nv):
         for sl in sl_to_show:
-            im__xy_to_show = im_naive_vz_xy[vv, sl, ...]
-            pl.ImagePlot(im__xy_to_show / np.max(im__xy_to_show), z=-3, x=-2, y=-1, 
+            im_xy_to_show = im_naive_vzxy[vv, sl, ...]
+            pl.ImagePlot(im_xy_to_show / np.max(im_xy_to_show), x=-2, y=-1, 
                         title='density compensated sos recon vol ' + str(vv) + ' all echos ', vmax=.7)
 # -
 
@@ -159,11 +165,11 @@ if is_jupyter:
     for vv in range(nv):
         plt.figure()
         plt.subplot(211)
-        plt.imshow(np.squeeze(np.abs(mask_vz_sxy[vv, 0, 0, ...])))
+        plt.imshow(np.squeeze(np.abs(mask_vzsxy[vv, 0, ...])))
         plt.xlabel('shots')
         plt.ylabel('ky')
         plt.subplot(212)    
-        plt.plot(np.squeeze(np.sum(np.abs(mask_vz_sxy[vv, 0, 0, ...]), 0)), 'x')
+        plt.plot(np.squeeze(np.sum(np.abs(mask_vzsxy[vv, 0, ...]), 0)), 'x')
         plt.ylim([-.1, None])
         plt.show()
 
@@ -174,26 +180,26 @@ ncalibx, ncaliby = 24, min(24, sequence_opts["cal_shot_split"] * sequence_opts["
 ncc = max(nc // 3, min(8, nc))
 
 if do_cc:
-    ksp_center_zxyc = sp.resize(ksp_cal_sz_xyc[0, :, 0, ...], (nz, ncalibx, ncaliby, nc))
-    ksp_cc_vz_sxyc = np.zeros((nv, nz, 1, ns, nx, ny, ncc), dtype=xp.complex64)
-    ksp_cc_cal_sz_xyc = np.zeros((n_cal_shots, nz, 1, nx, ny, ncc), dtype=xp.complex64)
+    ksp_center_zxyc = sp.resize(ksp_cal_szxyc[0, ...], (nz, ncalibx, ncaliby, nc))
+    ksp_cc_vzsxyc = np.zeros((nv, nz, ns, nx, ny, ncc), dtype=xp.complex64)
+    ksp_cc_cal_szxyc = np.zeros((n_cal_shots, nz, nx, ny, ncc), dtype=xp.complex64)
         
     for z in range(nz):
         cc_matrix = get_cc_matrix(ksp_center_zxyc[z, ...], ncc, do_plot=False)
-        ksp_cc_vz_sxyc[:, z, ...] = apply_cc_matrix(ksp_vz_sxyc[:, z, ...], cc_matrix)
-        ksp_cc_cal_sz_xyc[:, z, ...] = apply_cc_matrix(ksp_cal_sz_xyc[:, z, ...], cc_matrix)
+        ksp_cc_vzsxyc[:, z, ...] = apply_cc_matrix(ksp_vzsxyc[:, z, ...], cc_matrix)
+        ksp_cc_cal_szxyc[:, z, ...] = apply_cc_matrix(ksp_cal_szxyc[:, z, ...], cc_matrix)
 
-    ksp_vz_sxyc = sp.to_device(ksp_cc_vz_sxyc, recon_device)
-    ksp_cal_sz_xyc = sp.to_device(ksp_cc_cal_sz_xyc, recon_device)
+    ksp_vzsxyc = sp.to_device(ksp_cc_vzsxyc, recon_device)
+    ksp_cal_szxyc = sp.to_device(ksp_cc_cal_szxyc, recon_device)
     print('Coil compression from ', str(nc), ' to ', str(ncc))
     nc = ncc
 else:
-    ksp_vz_sxyc = sp.to_device(ksp_vz_sxyc, recon_device)
-    ksp_cal_sz_xyc = sp.to_device(ksp_cal_sz_xyc, recon_device)
+    ksp_vzsxyc = sp.to_device(ksp_vzsxyc, recon_device)
+    ksp_cal_szxyc = sp.to_device(ksp_cal_szxyc, recon_device)
     print('Skipped coil compression')
 
 
-ksp_density_compensated_vzexyc = np.sum(sp.to_device(ksp_vz_sxyc, sp.cpu_device) / dcf_vz__xy_, axis=3) # keep on cpu to save memory
+ksp_density_compensated_vzxyc = np.sum(sp.to_device(ksp_vzsxyc, sp.cpu_device) / dcf_vz_xy_, axis=2) # keep on cpu to save memory
 
 # +
 # estimate coil sensitivity
@@ -209,11 +215,11 @@ print('Do calib window ' + str(do_calib_window))
 cal_data_window_xy = get_window(WindowType.BOX, sens_calib_size, (nx, ny)).reshape(1, 1, 1, nx, ny) if do_calib_window else 1.
 
 if sens_estimation_flag == SensitivityDataSource.FULL_LOW_BVALUE_IMAGE:
-    ksp_for_sens_estimation_zcxy = np.transpose(ksp_density_compensated_vzexyc[0, :, 0, ...], (0, 3, 1, 2))
+    ksp_for_sens_estimation_zcxy = np.transpose(ksp_density_compensated_vzxyc[0, ...], (0, 3, 1, 2))
     ksp_for_sens_estimation_szcxy = np.tile(ksp_for_sens_estimation_zcxy[xp.newaxis, ...], (n_cal_shots, 1, 1, 1, 1))
 
 elif sens_estimation_flag == SensitivityDataSource.CALIBRATION_SHOTS:    
-    ksp_for_sens_estimation_szcxy = np.transpose(ksp_cal_sz_xyc[:, :, 0, ...], (0, 1, 4, 2, 3)) 
+    ksp_for_sens_estimation_szcxy = np.transpose(ksp_cal_szxyc, (0, 1, 4, 2, 3)) 
     
 sens_calzcxy = xp.zeros((n_cal_shots, nz, nc, nx, ny), dtype=np.complex64)
 for cal in range(n_cal_shots):
@@ -226,7 +232,7 @@ im_mask_calzxy = sos(sens_calzcxy, axis_in=2) > 0 # compute mask as union of bot
 im_mask_zxy = xp.sum(im_mask_calzxy, 0, keepdims=False) == im_mask_calzxy.shape[0]
 
 sens_zcxy_for_recon = sens_calzcxy[-1, ...] * im_mask_zxy[:, np.newaxis, ...]
-ksp_cc_zxyc_b0 = sp.to_device(ksp_density_compensated_vzexyc[0, :, 0, ...], recon_device)
+ksp_cc_zxyc_b0 = sp.to_device(ksp_density_compensated_vzxyc[0, ...], recon_device)
 im_conj_phase_zxy_b0 = xp.sum(sp.ifft(ksp_cc_zxyc_b0, axes=(-2, -3)) * xp.conj(xp.transpose(sens_zcxy_for_recon, (0, 2, 3, 1))), axis=-1)
 
 if is_jupyter:
@@ -238,7 +244,8 @@ if is_jupyter:
     pl.ImagePlot(np.angle(sens_to_show[sl_to_plot, :, ...]), z=-3, x=-2, y=-1, title='angle sens')
 
     pl.ImagePlot(im_mask_zxy, z=-3, x=-2, y=-1)
-    pl.ImagePlot(np.abs(im_conj_phase_zxy_b0), z=-3, x=-2, y=-1, vmax=.25*np.max(np.abs(im_conj_phase_zxy_b0)), title='density compensated conjugate phase coil combination')
+    pl.ImagePlot(np.abs(im_conj_phase_zxy_b0), z=-3, x=-2, y=-1, vmax=.75*np.max(np.abs(im_conj_phase_zxy_b0)), 
+                 title='density compensated conjugate phase coil combination')
 
 # +
 # reconstruct phase navigators
@@ -252,29 +259,28 @@ print('Phasenav window ' + str(phasenav_window_type) + ' window size ' + str((ph
 
 phasenav_window = sp.to_device(get_window(phasenav_window_type, (phasenav_window_x, phasenav_window_y), (nx, ny)), device=recon_device)
 
-im_phasenav_vz_sxy = xp.zeros((nv, nz, 1, ns, nx, ny), dtype=np.complex64)
+im_phasenav_vzsxy = xp.zeros((nv, nz, ns, nx, ny), dtype=np.complex64)
 
 sens_downsample_zcxy = sp.ifft(sp.resize(sp.fft(sens_zcxy_for_recon, axes=(-2, -1)), (nz, nc, phasenav_recon_x, phasenav_recon_y)), axes=(-2, -1)) 
 
 for v in range(nv):
     valid_shot_slicer = valid_shot_slicer_v[v]
-    ksp_z_sxyc = ksp_vz_sxyc[v, ...]
+    ksp_zsxyc = ksp_vzsxyc[v, ...]
     # reconstuct only low-res data
-    ksp_lowres_z_sxyc = sp.resize(ksp_z_sxyc, (nz, 1, ns, phasenav_recon_x, phasenav_recon_y, nc))    
-    ksp_lowres_z_scxy = xp.ascontiguousarray(xp.transpose(ksp_lowres_z_sxyc, (0, 1, 2, 5, 3, 4)))
+    ksp_lowres_zsxyc = sp.resize(ksp_zsxyc, (nz, ns, phasenav_recon_x, phasenav_recon_y, nc))    
+    ksp_lowres_zscxy = xp.ascontiguousarray(xp.transpose(ksp_lowres_zsxyc, (0, 1, 4, 2, 3)))
 
     for z in range(nz):
         sens_downsampled_cxy = sens_downsample_zcxy[z, ...]
-        ksp_lowres_scxy = ksp_lowres_z_scxy[z, 0, ...]
+        ksp_lowres_scxy = ksp_lowres_zscxy[z, ...]
         for shot in valid_shot_slicer: # only reconstruct valid shots
             ksp_lowres_cxy = ksp_lowres_scxy[shot, ...]
             app = mr.app.SenseRecon(ksp_lowres_cxy, sens_downsampled_cxy, device=recon_device, max_iter=5, show_pbar=False)
             im_phasenav_lowres_xy = app.run()
             ksp_phasenav_lowres_xy = sp.resize(sp.fft(im_phasenav_lowres_xy, axes=(-1, -2)), (nx, ny)) * phasenav_window
-            im_phasenav_vz_sxy[v, z, 0, shot, ...] = sp.ifft(ksp_phasenav_lowres_xy, axes=(-1, -2))
+            im_phasenav_vzsxy[v, z, shot, ...] = sp.ifft(ksp_phasenav_lowres_xy, axes=(-1, -2))
 
-im_phasenav_vzsxy = im_phasenav_vz_sxy[:, :, 0, ...] * im_mask_zxy[np.newaxis, :, np.newaxis, ...]
-
+im_phasenav_vzsxy = im_phasenav_vzsxy * im_mask_zxy[np.newaxis, :, np.newaxis, ...]
 
 
 # +
@@ -324,9 +330,8 @@ if is_jupyter:
 
 # +
 # combine shots recon
-im_vz_xy_recon = xp.zeros((nv, nz, 1, nx, ny), dtype=xp.complex64)
-mask_vz_sxy = sp.to_device(mask_vz_sxy, recon_device)
-mask_vzs_xy = xp.transpose(mask_vz_sxy, (0, 1, 3, 2, 4, 5))
+im_vzxy_recon = xp.zeros((nv, nz, nx, ny), dtype=xp.complex64)
+mask_vzsxy = to_recon_device(mask_vzsxy)
 
 im_phasenav_for_recon_vzsxy = xp.copy(im_phasenav_vzsxy)
 im_phasenav_for_recon_vzsxy[0, ...] = xp.ones((nz, ns, nx, ny), dtype=np.complex64)
@@ -340,44 +345,45 @@ for vv in range(0, nv):
     valid_shot_slicer = valid_shot_slicer_v[vv]
 
     # need to remove volume first otherwise shots gets bumped to front when indexing with list object, this doesn't happen when using slice object
-    ksp_zcs_xy = xp.transpose(ksp_vz_sxyc[vv][:, :, valid_shot_slicer, ...], (0, 5, 2, 1, 3, 4))
+    ksp_zsxyc = ksp_vzsxyc[vv]
+    ksp_zcsxy = xp.transpose(ksp_zsxyc[:, valid_shot_slicer, ...], (0, 4, 1, 2, 3))
     im_phasenav_zsxy = im_phasenav_for_recon_vzsxy[vv][:, valid_shot_slicer, ...]  
-    mask_zs_xy = mask_vzs_xy[vv][:, valid_shot_slicer, ...]      
+    mask_zsxy = mask_vzsxy[vv][:, valid_shot_slicer, ...]      
 
     if do_magnitude_shot_weighting and vv > 0:
         im_phasenav_magnitude_zsxy = im_normalized_phasenav_weights_vzsxy[vv][:, valid_shot_slicer, ...]
     else:
         im_phasenav_magnitude_zsxy = None
 
-    im_vz_xy_recon[vv, ...], _ = fista_2d_fse_recon(ksp_zcs_xy, im_phasenav_zsxy, sens_zcxy_for_recon, mask_zs_xy, 
+    im_vzxy_recon[vv, ...], _ = fista_2d_fse_recon(ksp_zcsxy, im_phasenav_zsxy, sens_zcxy_for_recon, mask_zsxy, 
                                 im_phasenav_magnitude_zsxy, max_iter=50, lamda=lambda_wav_independent)
 # -
 
 
 if is_jupyter:
-    pl.ImagePlot(im_vz_xy_recon[0, 0::2, 0, ...].get(), z=-3, title='recon b0 images')
+    pl.ImagePlot(im_vzxy_recon[0, 0::2, ...].get(), z=-3, title='recon b0 images')
 
     for z in range(1, nz, 2):
-        slicer_a = (0, z, 0, slice(None), slice(None))
-        slicer_b = (1, z, 0, slice(None), slice(None))
-        pl.ImagePlot(np.concatenate((im_vz_xy_recon[slicer_a].get(), 2 * im_vz_xy_recon[slicer_b].get()), axis=1), title='slice ' + str(z))
+        slicer_a = (0, z, slice(None), slice(None))
+        slicer_b = (1, z, slice(None), slice(None))
+        pl.ImagePlot(np.concatenate((im_vzxy_recon[slicer_a].get(), 2 * im_vzxy_recon[slicer_b].get()), axis=1), title='slice ' + str(z))
 
 # generate adc map
 diff_bval = sequence_opts["bvalue"] - sequence_opts["first_volume_bvalue"]
-im_vzxy_recon = np.squeeze(np.abs(im_vz_xy_recon.get()))
+im_vzxy_recon = np.squeeze(np.abs(im_vzxy_recon.get()))
 adc_map_zxy = calculate_adc_map(im_vzxy_recon[0, ...], im_vzxy_recon[1, ...], diff_bval) * im_mask_zxy.get()
 
 # +
 get_tag = lambda: ''
 print('saving to ', recon_out_dir)
 
-cfl_exchanger.save_clinic_cfls(recon_out_dir, [im_vz_xy_recon.get(), adc_map_zxy])
+cfl_exchanger.save_clinic_cfls(recon_out_dir, list(map(to_cpu, [im_vzxy_recon, adc_map_zxy])))
 
 # save memory by only saving reduced number of shots if reconstructing subsets of data
 do_save_reduced_number_of_shots = len(valid_shot_slicer_v[-1]) < ns
 vols_to_save = [-1] if do_save_reduced_number_of_shots else slice(None, None, None)
 shots_to_save = valid_shot_slicer_v[-1] if do_save_reduced_number_of_shots else slice(None, None, None)
-cfl_exchanger.save_debug_cfls(recon_out_dir, [im_phasenav_vzsxy[vols_to_save, :, shots_to_save, ...].get(), 
-                                                im_normalized_phasenav_weights_vzsxy[vols_to_save, :, shots_to_save, ...].get(), 
-                                                im_phasenav_weights_vzsxy[vols_to_save, :, shots_to_save, ...].get(),
-                                                sens_calzcxy.get()])
+cfl_exchanger.save_debug_cfls(recon_out_dir, list(map(to_cpu, [im_phasenav_vzsxy[vols_to_save, :, shots_to_save, ...], 
+                                                im_normalized_phasenav_weights_vzsxy[vols_to_save, :, shots_to_save, ...], 
+                                                im_phasenav_weights_vzsxy[vols_to_save, :, shots_to_save, ...],
+                                                sens_calzcxy])))
